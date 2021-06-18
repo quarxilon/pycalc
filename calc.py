33f4e6672650402a21a08dfe5f1a2787c23f0620
quarxilon <brandonkbg@gmail.com>
2021-06-18 09:46:55 +0800

    Edit docstring to match PEP257 style guide

--- calc.py
+++ calc.py
@@ -1,6 +1,5 @@
-# Perform simple arithmetic encoded in an input string:
-# '1 + 2' -> 3, or '1 - 2' -> -1.
 def compute(expression):
+    """Perform simple arithmetic on two operands encoded in an input string."""
     num0, operator, num1 = expression.split(' ')
     num0, num1 = int(num0), int(num1)
     if operator == '+':
